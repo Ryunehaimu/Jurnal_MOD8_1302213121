ead9193f127427e3584df11fd6ae960bea015f52
Ryunehaimu <108001133+Ryunehaimu@users.noreply.github.com>
2023-04-12 17:02:37 +0700

    aplikasi bank transfer dengan runtime configuration.

---
+++ ConsoleApp1/ConsoleApp1/BankTranferConfig.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class BankTranferConfig
+    {
+        private List<string> methods;
+
+        public bankconfig bank { get; set; }
+        public BankTranferConfig()
+        {
+            try
+            {
+                ReadJson();
+            }
+            catch
+            {
+                setDefault();
+                writeJson();
+            }
+        }
+
+        public BankTranferConfig(bankconfig bank)
+        {
+            this.bank = bank;
+        }
+        public void ReadJson()
+        {
+            string hasil = File.ReadAllText(@"./bank_transfer_config.json");
+            bank = JsonSerializer.Deserialize<bankconfig>(hasil);
+        }
+
+        public void setDefault()
+        {
+            List<String> list = new List<String> { "RTO(real-time)", "SKN", "RTGS","BI FAST"};
+            bank = new bankconfig("en", 25000000, 6500, 15000, methods, "yes", "ya");
+        }
+        public void writeJson()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+            String jsonstr = JsonSerializer.Serialize(bank, options);
+            File.WriteAllText(@"./bank_tranfer_config.json",jsonstr);
+        }
+
+        public class bankconfig
+        {
+            public String lang { get; set; }
+            public transfer transfer { get; set; }
+            public List<String> methods { get; set; }
+            public confirmation confirmation { get; set; }
+            public bankconfig() { }
+            public bankconfig(String lang, int treshold, int low_fee, int high_fee, List<String> methods, String en, String id)
+            {
+                this.lang = lang;
+                transfer = new transfer(treshold,low_fee,high_fee);
+                this.methods = methods;
+                confirmation = new confirmation(en,id);
+            }
+        }
+   
+        public class transfer
+        {
+            public int threshold { get; set; }
+            public int low_fee { get; set; }
+            public int high_fee { get; set; }
+            public transfer(int treshold,int low_fee,int high_fee)
+            {
+                this.threshold = treshold;
+                this.low_fee = low_fee;
+                this.high_fee = high_fee;
+            }
+      
+        }
+
+        public class confirmation
+        {
+            public String en { get; set; }
+            public String id { get; set; }
+
+            public confirmation(String en, String id)
+            {
+                this.en = en;
+                this.id = id;
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+using System.Net.Http.Headers;
+
+BankTranferConfig config = new BankTranferConfig();
+Console.WriteLine("Pilih Bahasa(en/id)");
+config.bank.lang = Console.ReadLine();
+if (config.bank.lang == "en")
+{
+    Console.Write("Please insert the amount of money to transfer: ");
+    String transfer = Console.ReadLine();
+    int nominal = Convert.ToInt32(transfer);
+    if (transfer <= config.bank.transfer.threshold)
+    {
+        int totaltranfer = nominal + config.bank.transfer.low_fee;
+        Console.WriteLine("Transfer fee: " + config.bank.transfer.low_fee);
+        Console.WriteLine("Total amount : " + totaltranfer);
+    }
+    else
+    {
+        int totaltranfer = nominal + config.bank.transfer.high_fee;
+        Console.WriteLine("Transfer fee: " + config.bank.transfer.high_fee);
+        Console.WriteLine("Total amount : " + totaltranfer);
+    }
+
+    Console.WriteLine("Select tranfer method:");
+    Console.WriteLine("1. RTO(real-time)");Console.WriteLine("2. SKN");Console.WriteLine("3. RTGS");Console.WriteLine("4. BI FAST");
+    String methods = Console.ReadLine();
+    config.bank.methods[0] = methods;
+}
+else
+{
+    Console.Write("Masukan jumlah uang yang akan di-transfer: ");
+    String transfer = Console.ReadLine();
+    int nominal = Convert.ToInt32(transfer);
+    if (transfer <= config.bank.transfer.threshold)
+    {
+        int totaltranfer = nominal + config.bank.transfer.low_fee;
+        Console.WriteLine("Biaya transfer: " + config.bank.transfer.low_fee);
+        Console.WriteLine("Total biaya: "+totaltranfer);
+    }
+    else
+    {
+        int totaltranfer = nominal + config.bank.transfer.high_fee;
+        Console.WriteLine("Biaya transfer: " + config.bank.transfer.high_fee);
+        Console.WriteLine("Total biaya: " + totaltranfer);
+    }
+    Console.WriteLine("Pilih metode tranfer:");
+    Console.WriteLine("1. RTO(real-time)"); Console.WriteLine("2. SKN"); Console.WriteLine("3. RTGS"); Console.WriteLine("4. BI FAST");
+    String methods = Console.ReadLine();
+    config.bank.methods[0] = methods;
+}
